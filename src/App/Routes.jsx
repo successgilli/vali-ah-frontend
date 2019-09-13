4e355f3543333de404b33562909f00d95737354e
nennyfills <nennydike@gmail.com>
2019-09-13 11:03:00 +0100

    feat(profile): user can view and update profile
    - design view profile page
    - design update profile page
    - implement API call using redux-saga to fetch and update user profile
    - create types and actions for user profile
    - write test for redux-reducers
    - write test for redux action
    - write test for redux-saga generators
    
    [Finishes #167377498]

--- src/App/Routes.jsx
+++ src/App/Routes.jsx
@@ -5,12 +5,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 // pages
-import HomePage from 'pages/HomePage';
 import PasswordResetPage from 'pages/PasswordResetPage';
 import UserFeedPage from 'pages/UserFeedPage';
 import NotFoundPage from 'pages/NotFoundPage';
 import ProfileView from 'pages/ProfileView';
 import ProfileUpdatePage from 'pages/ProfileUpdate';
+import HomePage from '../pages/HomePage/index';
 
 
 const Routes = () => (
